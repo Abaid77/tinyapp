94065954345bf90b67d35fb9a996075619b3923a
Amit Baid <baidstar@hotmail.com>
2021-08-04 00:23:52 -0600

    Added a logout button when logged in

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan")
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+
+
 
 function generateRandomString() {
   let string = "";
@@ -28,12 +31,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(morgan("dev"))
 
+app.use(cookieParser())
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/login", (req, res) => {
+  const userVal = req.body.username
+  res.cookie("username", userVal);
+  res.redirect("/urls")
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls")
+})
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -45,7 +64,10 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -63,7 +85,11 @@ app.post("/urls/:shortURL/update", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
